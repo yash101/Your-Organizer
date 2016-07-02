8c5484f6f977093a3ac7f53fe753fc067aa0ed4a
Yash <lodhad@gmail.com>
2016-07-02 00:32:27 -0700

    Removed SSL code. Starting fresh.

--- httpserver.cpp
+++ httpserver.cpp
@@ -4,48 +4,15 @@
 
 void http::HttpServer::start()
 {
-  if(me.sslEnable)
-  {
-    boost::asio::ssl::context sslContext(me.ioService, boost::asio::ssl::context::sslv23);
-    boost::asio::ip::tcp::acceptor acceptor(me.ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v6(), me.listeningPort));
-
-    sslContext.set_options(
-        boost::asio::ssl::context::default_workarounds |
-        boost::asio::ssl::context::no_sslv2 |
-        boost::asio::ssl::context::single_dh_use
-    );
-
-    sslContext.set_password_callback(boost::bind(&http::HttpServer::get_password, this));
-    sslContext.use_certificate_chain_file(me.certificateChainFile);
-    sslContext.use_private_key_file(me.privateKeyFile, boost::asio::ssl::context::pem);
-    sslContext.use_tmp_dh_file(me.temporaryDhFile);
-
-    while(true)
-    {
-      sock::SocketPointer socket(new boost::asio::ip::tcp::socket(me.ioService));
-      acceptor.accept(*socket);
-      boost::thread thread(boost::bind(&http::HttpServer::runner, this, socket, sslContext));
-      thread.detach();
-    }
-  }
 }
 
 void http::HttpServer::runner()
 {
-  try
-  {
-  }
-  catch(std::exception& e)
-  {}
 }
 
-void http::HttpServer::parseHttpRequest(boost::asio::ip::tcp::socket& socket, boost::asio::ssl::context& context)
+void http::HttpServer::parseHttpRequest(boost::asio::ip::tcp::socket& socket)
 {
-  if(me.sslEnable)
-  {
-    http::SslSocket socket(me.ioService, context);
-    socket.lowest_layer() = socket;
-  }
+  socket.write_some(boost::asio::buffer("Hello World!\n"));
 }
 
 
@@ -53,8 +20,7 @@ void http::HttpServer::parseHttpRequest(boost::asio::ip::tcp::socket& socket, bo
 
 http::HttpServer::HttpServer() :
   listeningPort(0),
-  isServerRunning(false),
-  sslEnable(false)
+  isServerRunning(false)
 {}
 
 http::HttpServer::~HttpServer()
@@ -73,84 +39,4 @@ short http::HttpServer::setPort(short port)
 {
   ERRR
   return (me.listeningPort = port);
-}
-
-bool http::HttpServer::enableSSL()
-{
-  ERRR
-  bool ret = me.sslEnable;
-  sslEnable = true;
-  return ret;
-}
-
-bool http::HttpServer::disableSSL()
-{
-  ERRR
-  bool ret = me.sslEnable;
-  sslEnable = false;
-  return ret;
-}
-
-bool http::HttpServer::isSSLEnabled()
-{
-  return sslEnable;
-}
-
-std::string http::HttpServer::setPassword(std::string pass)
-{
-  ERRR;
-  SET(me.password, pass);
-}
-
-std::string http::HttpServer::getPassword()
-{
-  GET(me.password);
-}
-
-std::string http::HttpServer::setCertificateChainFile(std::string path)
-{
-  ERRR;
-  SET(me.certificateChainFile, path);
-}
-
-std::string http::HttpServer::getCertificateChainFile()
-{
-  GET(me.certificateChainFile);
-}
-
-std::string http::HttpServer::setPrivateKeyFile(std::string path)
-{
-  ERRR;
-  SET(me.privateKeyFile, path);
-}
-
-std::string http::HttpServer::getPrivateKeyFile()
-{
-  GET(me.privateKeyFile);
-}
-
-std::string http::HttpServer::setTemporaryDhFile(std::string path)
-{
-  ERRR;
-  SET(me.temporaryDhFile, path);
-}
-
-std::string http::HttpServer::getTemporaryDhFile()
-{
-  GET(me.temporaryDhFile);
-}
-
-void http::HttpServer::enableSafeMode()
-{
-  safeMode = true;
-}
-
-bool http::HttpServer::isSafeModeEnabled()
-{
-  return safeMode;
-}
-
-std::string http::HttpServer::get_password() const
-{
-  return me.password;
 }
--- httpserver.h
+++ httpserver.h
@@ -11,13 +11,12 @@ namespace http
 {
   class HttpServer;
 
-  typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket> SslSocket;
   typedef boost::asio::ip::tcp::socket Socket;
 
   class HttpSocketSession
   {
   public:
-    boost::asio::ip::socket Socket;
+    Socket& socket;
   };
 
   class HttpServer
@@ -25,19 +24,13 @@ namespace http
   private:
 
     boost::asio::io_service ioService;
-    bool isServerRunning;
-    bool sslEnable;
     short listeningPort;
-    bool safeMode; 				//Prevents retrieval of certificate data from class
-    std::string password;
-    std::string certificateChainFile;
-    std::string privateKeyFile;
-    std::string temporaryDhFile;
+    bool isServerRunning;
 
     std::string get_password() const;
     void acceptor();
     void runner();
-    void parseHttpRequest(boost::asio::ip::tcp::socket& sock, boost::asio::ssl::context& context);
+    void parseHttpRequest(boost::asio::ip::tcp::socket& sock);
 
   public:
 
@@ -48,20 +41,6 @@ namespace http
 
     short getPort();
     short setPort(short listeningPort);
-    bool enableSSL();
-    bool disableSSL();
-    bool isSSLEnabled();
-    std::string setPassword(std::string pass);
-    std::string getPassword();
-    std::string setCertificateChainFile(std::string path);
-    std::string getCertificateChainFile();
-    std::string setPrivateKeyFile(std::string path);
-    std::string getPrivateKeyFile();
-    std::string setTemporaryDhFile(std::string path);
-    std::string getTemporaryDhFile();
-
-    void enableSafeMode();
-    bool isSafeModeEnabled();
   };
 }
 
