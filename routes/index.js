fe3162f97e149285251cace05aaa6b12d6c2baa3
Yash <lodhad@gmail.com>
2016-07-09 23:17:23 -0700

    Working on creating a functional (ugly) sign up form and log in form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
+});
+router.get('/login', function(req, res, next) {
+  res.render('auth/login', { title: "Log In" });
+});
+router.get('/register', function(req, res, next) {
+  res.render('auth/register', { title: "Register" });
 });
 
 module.exports = router;
