833a2d7a56cfbad670d095969ac1306fb58dbe43
Yash <lodhad@gmail.com>
2016-07-12 23:19:36 -0700

    Preparing for PostgreSQL.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {});
 });
+
 router.get('/login', function(req, res, next) {
   res.render('auth/login', { title: "Log In" });
 });
+
 router.get('/register', function(req, res, next) {
   res.render('auth/register', { title: "Register" });
+});
+
+router.get('/auth/register', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/auth/register', function(req, res, next) {
 });
 
 module.exports = router;
